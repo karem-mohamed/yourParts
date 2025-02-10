cb8bde4ac60fa28113e56b80e1effea4c385bc6a
karem <karem96mohamed@gmail.com>
2025-02-10 18:19:34 +0200

    backend/add drizzle orm with drizzle-kit for migration , create db schema , add redis and create dokcer compose file for using postgres server and redis

---
+++ backend/drizzle.config.ts
@@ -0,0 +1,12 @@
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+config();
+
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./src/models/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
